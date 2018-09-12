f7c44bba0a169c82d865a83e7d09861cf0f06a81
p.olifer <pavel.olifer@gmail.com>
2018-09-12 22:54:45 +0300

    chapter3

---
+++ chapter3/main3.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+__author__ = 'p.olifer'
+
+import os
+
+print(os.getcwd())
+
+man = []
+other = []
+try:
+    data = open('sketch.txt')
+    for each_line in data:
+        try:
+            (role, line_spoken) = each_line.split(':', 1)
+            line_spoken = line_spoken.strip()
+            if role == 'Man':
+                man.append(line_spoken)
+            elif role == 'Other Man':
+                other.append(line_spoken)
+        except ValueError:
+            pass
+    data.close()
+except IOError:
+    print('The datafile is missing')
+print(man)
+print(other)
+
+try:
+    with open('man_data.txt', 'w') as man_file:
+        print(man, file=man_file)
+    with open('other_data.txt', 'w') as other_file:
+        print(other, file=other_file)
+except IOError as err:
+    print('File error: ' + str(err))
--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 __author__ = 'p.olifer'
 
+import sys
 
-def print_lol(the_list):
+movies = [
+    "The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+    ["Graham Chapman",
+     ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+def print_lol(the_list, indent=False, level=0, fn = sys.stdout):
     for each_item in the_list:
         if isinstance(each_item, list):
-            print_lol(each_item)
+            print_lol(each_item, indent, level+1, fn)
         else:
-            print(each_item)
+            if indent:
+                for tab_stop in range(level):
+                    print('\t', end='', file=fn)
+                print(each_item, file=fn)
+
+print_lol(movies, True)
