b76e5c20240c760f289c93f4d8eb6d851f2b678c
p.olifer <pavel.olifer@gmail.com>
2018-09-16 23:52:29 +0300

    webapp, fix generate_list.py

--- webapp/cgi-bin/generate_list.py
+++ webapp/cgi-bin/generate_list.py
@@ -8,7 +8,7 @@ import glob
 
 data_files = glob.glob("data/*.txt")
 athletes = athletemodel.put_to_store(data_files)
-print(yate.include_footer({"Home": "/index.html"}))
+
 print(yate.start_response())
 print(yate.include_header("Coach Kelly's List of Athletes"))
 print(yate.start_form("generate_timing_data.py"))
